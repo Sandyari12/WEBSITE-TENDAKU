1c1167ff610e65e4c2fa738b775bc8a3218520e9
Daksa1904 <ketutdaksatampiada19@gmail.com>
2025-07-03 15:06:34 +0800

    update

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import { useProducts } from '../context/ProductContext'
 import { Drawer, Button as AntButton, message, Modal } from 'antd'
+import { useNavigate } from 'react-router-dom'
 
 function Admin() {
   const [activeTab, setActiveTab] = useState('products')
   const { products, addProduct, editProduct, deleteProduct } = useProducts()
+  const navigate = useNavigate();
 
   const [orders, setOrders] = useState([
     {
@@ -173,6 +175,16 @@ function Admin() {
 
   return (
     <div className="max-w-6xl mx-auto">
+      {/* Tombol Kembali ke Beranda */}
+      <button
+        onClick={() => navigate('/')}
+        className="flex items-center gap-2 border border-gray-300 rounded-lg px-4 py-2 mb-4 hover:bg-gray-100 transition"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+        </svg>
+        <span>Kembali ke Beranda</span>
+      </button>
       {/* Admin Header */}
       <div className="bg-[#2C3E50] rounded-xl p-6 mb-8 flex items-center justify-between shadow-lg">
         <div>
